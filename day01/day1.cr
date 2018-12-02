387dabc650679efad45532f5aa0664765874de8c
Lin Qiu <lin@upside.com>
2018-12-02 12:52:05 -0500

    Add day 1

---
+++ day01/day1.cr
@@ -0,0 +1,26 @@
+input = File.read_lines("./day1a.txt")
+
+sum = input.reduce(0) { |acc, n| acc + n.to_i }
+
+puts "part 1 answer: "
+puts sum
+
+# second part
+# two loops through
+repeat = nil
+hash = {} of Int32 => Int32
+init = 0
+
+while repeat.nil?
+  init = input.reduce(init) do |acc, n|
+    if hash[acc]?
+      hash[acc] = hash[acc] + 1
+      repeat ||= acc
+    else
+      hash[acc] = 1
+    end
+    acc + n.to_i
+  end
+end
+
+puts repeat
